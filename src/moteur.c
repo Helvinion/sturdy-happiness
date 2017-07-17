9665672d55b491e768e6d7ab2251399fdca60f9c
Helvinion <qmignot@gmail.com>
2017-07-17 23:35:28 +0200

    Amélioration de la gestion des animations

--- src/moteur.c
+++ src/moteur.c
@@ -14,12 +14,14 @@
 #define PPUDATA (unsigned char*)0x2007
 
 #define FRAME_COUNT (unsigned char*)0x00
+
 /*
 static struct element_physique *elements[10];
 static unsigned char nombre_elements;
 */
-static struct element_physique G_Jolivet_phys = {&G_Jolivet, 15,150, 0,0, 0,0, 0};
 
+static struct element_physique *joueur;
+static struct element_physique elements[10];
 
 void attendre_VBlank(void)
 {
@@ -33,6 +35,18 @@ void attendre_VBlank(void)
    return;
 }
 
+static void traiter_animations()
+{
+	static unsigned char compteur = 0;
+	
+	if (compteur++ == 32)
+	{
+		// Rustique. Animer tous les 8 frames.
+		animer(joueur->dessin);
+		compteur = 0;
+	}
+}
+
 static void debut_boucle()
 {
 	update_manettes();
@@ -43,11 +57,12 @@ static void debut_boucle()
 static void fin_boucle()
 {
 	int i = 0;
-	
+
+	traiter_animations();
 //	while (i++ < nombre_elements)
 	{
-		appliquer_physique(&G_Jolivet_phys);
-		afficher_personnage(G_Jolivet_phys.dessin);
+		appliquer_physique(joueur);
+		afficher_personnage(joueur->dessin);
 	}
 	sprites_end();	
 }
@@ -55,10 +70,11 @@ static void fin_boucle()
 void initialiser()
 {
 	int i = 0;
-		
+	static struct element_physique G_Jolivet_phys = {&G_Jolivet, 15,150, 0,0, 0,0, 0};
+
 	(*PPUCTRL) = (unsigned char)0x90;
    
-    // change palette during vblank
+    // changer les palettes pendant un vblank
     attendre_VBlank();
 
     for (i = 0; i < 8; i++)
@@ -71,43 +87,35 @@ void initialiser()
    
     tiles_init();
 	
+	joueur = &G_Jolivet_phys;
+	
 	attendre_VBlank();
 }
 
 void moteur_mode_jeu()
 {
-	static unsigned char i = 0;
 	debut_boucle();
 	
 	if (bouton_presse_manette_1(BOUTON_DROITE))
 	{
-		i++;
-		changer_animation(&G_Jolivet, 1);
-		G_Jolivet_phys.vitesse_x = 2;
+		changer_animation(joueur->dessin, 1);
+		joueur->vitesse_x = 1;
 	}
 	else if (bouton_presse_manette_1(BOUTON_GAUCHE))
 	{
-		i++;
-		changer_animation(&G_Jolivet, 1);
-		G_Jolivet_phys.vitesse_x = -2;
+		changer_animation(joueur->dessin, 1);
+		joueur->vitesse_x = -1;
 	}
 	else
 	{
-		i = 0;
-		changer_animation(&G_Jolivet, 0);
-		G_Jolivet_phys.vitesse_x = 0;
+		changer_animation(joueur->dessin, 0);
+		joueur->vitesse_x = 0;
 	}
 	
 	if (nouveau_bouton_presse_manette_1(BOUTON_A))
 	{
-		saut(&G_Jolivet_phys);
+		saut(joueur);
 	}
-	
-	if (i == 5)
-	{
-		animer(&G_Jolivet);
-		i = 0;
-	}
-	
+
 	fin_boucle();
 }
